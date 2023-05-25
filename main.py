704079bd24fc6aac900a0f47f7b42e0efe1fc8b4
dile0 <dile@ycp.edu>
2023-05-25 16:46:52 -0400

    Adding Initial Calculator Information

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import argparse
+
+def startUpPrompt():
+
+    user_type = 0
+    print("Welcome to the Fundamental Calculator!\n")
+    print("\n")    
+    #Insert some fun text art for a calcuator here
+    print("What type of computation would you like to perform?\n")
+
+    while user_type < 1 or user_type > 7: 
+
+        print("Type 1: Basic Computation")
+        print("Type 2: Trigonometric Functions")
+        print("Type 3: Logarithmic Functions")
+        print("Type 4: Factorials, Absolutes, Permutations & Combinations")
+        print("Type 5: Hyperbolic Functions")
+        print("Type 6: Statistic Functions")
+        print("Type 7: Unit Conversions\n")
+        user_type = input("Please enter a type number:")
+        user_type = int(user_type)
+        
+        if user_type < 1 or user_type > 7:
+            print("Invalid choice. Please try again.")
+
+    return  user_type
+
+if __name__ == "__main__":
+    user_choice = startUpPrompt()
+    print("You chose type: ", user_choice)
