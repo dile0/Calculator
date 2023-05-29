3529a274cca6a093b37749566c9f43c1c318b3a6
Derek Ile <92697566+dile0@users.noreply.github.com>
2023-05-28 22:41:01 -0400

    Add files via upload

--- trigComputations.py
+++ trigComputations.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 # REWORK THIS TO SEND BACK INFORMATION TO THE MAIN.PY FILE
-# REWORK THIS TO ALLOW VALUES TO TRANSFER FROM PROCESS TO PROCESS AND RESET VALUES
 
 import math
 
@@ -33,7 +32,8 @@ def trigCompPrompt():
     return user_comp
 
 def cosFunc(optionalValue):
-    while True:
+    user_continue = "Y"
+    while user_continue == "Y":
         if optionalValue != 0.0:
             cos_choice = int(input("Do you want the previous value to be the adjacent side [1], hypotenuse side [2], or neither? [3]"))
             match cos_choice:
@@ -46,16 +46,22 @@ def cosFunc(optionalValue):
                 case _:
                     adjSide = float(input("Please enter a value for the adjacent side: "))
                     hypoSide = float(input("Please enter a value for the hypotenuse side: "))
+        else:
+            adjSide = float(input("Please enter a value for the adjacent side: "))
+            hypoSide = float(input("Please enter a value for the hypotenuse side: "))
         cosSolution = float(math.cos(adjSide/hypoSide))
 
         print("The theta (θ) of ", adjSide, " and ", hypoSide, " is ", cosSolution, " radians.")
 
+        optionalValue = cosSolution
+
         user_continue = input("Would you like to continue? Y or N\n")
         if user_continue == "N":
-            break;
+            return cosSolution
 
 def sinFunc(optionalValue):
-    while True:
+    user_continue = "Y"
+    while user_continue == "Y":
         if optionalValue != 0.0:
             sin_choice = int(input("Do you want the previous value to be the opposite side [1], hypotenuse side [2], or neither? [3]"))
             match sin_choice:
@@ -68,16 +74,22 @@ def sinFunc(optionalValue):
                 case _:
                     oppSide = float(input("Please enter a value for the opposite side: "))
                     hypoSide = float(input("Please enter a value for the hypotenuse side: "))
+        else:
+            oppSide = float(input("Please enter a value for the opposite side: "))
+            hypoSide = float(input("Please enter a value for the hypotenuse side: "))
         sinSolution = float(math.sin(oppSide/hypoSide))
 
         print("The theta (θ) of ", oppSide, " and ", hypoSide, " is ", sinSolution, " radians.")
 
+        optionalValue = sinSolution
+
         user_continue = input("Would you like to continue? Y or N\n")
         if user_continue == "N":
-            break;
+            return sinSolution
 
 def tanFunc(optionalValue):
-    while True:
+    user_continue = "Y"
+    while user_continue == "Y":
         if optionalValue != 0.0:
             tan_choice = int(input("Do you want the previous value to be the opposite side [1], adjacent side [2], or neither? [3]"))
             match tan_choice:
@@ -90,16 +102,22 @@ def tanFunc(optionalValue):
                 case _:
                     oppSide = float(input("Please enter a value for the opposite side: "))
                     adjSide = float(input("Please enter a value for the adjacent side: "))
+        else:
+            oppSide = float(input("Please enter a value for the opposite side: "))
+            adjSide = float(input("Please enter a value for the adjacent side: "))
         tanSolution = float(math.tan(oppSide/adjSide))
 
         print("The theta (θ) of ", oppSide, " and ", adjSide, " is ", tanSolution, " radians.")
 
+        optionalValue = tanSolution
+
         user_continue = input("Would you like to continue? Y or N\n")
         if user_continue == "N":
-            break;
+            return tanSolution
 
 def secFunc(optionalValue):
-    while True:
+    user_continue = "Y"
+    while user_continue == "Y":
         if optionalValue != 0.0:
             sec_choice = int(input("Do you want the previous value to be the adjacent side [1], hypotenuse side [2], or neither? [3]"))
             match sec_choice:
@@ -112,16 +130,22 @@ def secFunc(optionalValue):
                 case _:
                     adjSide = float(input("Please enter a value for the adjacent side: "))
                     hypoSide = float(input("Please enter a value for the hypotenuse side: "))
+        else:
+            adjSide = float(input("Please enter a value for the adjacent side: "))
+            hypoSide = float(input("Please enter a value for the hypotenuse side: "))
         secSolution = float(1/math.cos(adjSide/hypoSide))
 
         print("The theta (θ) of ", hypoSide, " and ", adjSide, " is ", secSolution, " radians.")
 
+        optionalValue = secSolution
+
         user_continue = input("Would you like to continue? Y or N\n")
         if user_continue == "N":
-            break;
+            return secSolution
 
 def cscFunc(optionalValue):
-    while True:
+    user_continue = "Y"
+    while user_continue == "Y":
         if optionalValue != 0.0:
             csc_choice = int(input("Do you want the previous value to be the opposite side [1], hypotenuse side [2], or neither? [3]"))
             match csc_choice:
@@ -134,16 +158,22 @@ def cscFunc(optionalValue):
                 case _:
                     oppSide = float(input("Please enter a value for the opposite side: "))
                     hypoSide = float(input("Please enter a value for the hypotenuse side: "))
+        else:
+            oppSide = float(input("Please enter a value for the opposite side: "))
+            hypoSide = float(input("Please enter a value for the hypotenuse side: "))
         cscSolution = float(1/math.sin(oppSide/hypoSide))
 
         print("The theta (θ) of ", oppSide, " and ", hypoSide, " is ", cscSolution, " radians.")
 
+        optionalValue = cscSolution
+
         user_continue = input("Would you like to continue? Y or N\n")
         if user_continue == "N":
-            break;
+            return cscSolution
 
 def cotFunc(optionalValue):
-    while True:
+    user_continue = "Y"
+    while user_continue == "Y":
         if optionalValue != 0.0:
             cot_choice = int(input("Do you want the previous value to be the opposite side [1], adjacent side [2], or neither? [3]"))
             match cot_choice:
@@ -156,16 +186,22 @@ def cotFunc(optionalValue):
                 case _:
                     oppSide = float(input("Please enter a value for the opposite side: "))
                     adjSide = float(input("Please enter a value for the adjacent side: "))
+        else:
+            oppSide = float(input("Please enter a value for the opposite side: "))
+            adjSide = float(input("Please enter a value for the adjacent side: "))
         cotSolution = float(1/math.tan(oppSide/adjSide))
 
         print("The theta (θ) of ", oppSide, " and ", adjSide, " is ", cotSolution, " radians.")
 
+        optionalValue = cotSolution
+
         user_continue = input("Would you like to continue? Y or N\n")
         if user_continue == "N":
-            break;
+            return cotSolution
 
 def acosFunc(optionalValue):
-    while True:
+    user_continue = "Y"
+    while user_continue == "Y":
         if optionalValue != 0.0:
             acos_choice = int(input("Do you want the previous value to be the adjacent side [1], hypotenuse side [2], or neither? [3]"))
             match acos_choice:
@@ -178,16 +214,22 @@ def acosFunc(optionalValue):
                 case _:
                     adjSide = float(input("Please enter a value for the adjacent side: "))
                     hypoSide = float(input("Please enter a value for the hypotenuse side: "))
+        else:
+            adjSide = float(input("Please enter a value for the adjacent side: "))
+            hypoSide = float(input("Please enter a value for the hypotenuse side: "))
         acosSolution = float(math.acos(adjSide/hypoSide))
 
         print("The theta (θ) of ", adjSide, " and ", hypoSide, " is ", acosSolution, " radians.")
 
+        optionalValue = acosSolution
+
         user_continue = input("Would you like to continue? Y or N\n")
         if user_continue == "N":
-            break;
+            return acosSolution
 
 def asinFunc(optionalValue):
-    while True:
+    user_continue = "Y"
+    while user_continue == "Y":
         if optionalValue != 0.0:
             asin_choice = int(input("Do you want the previous value to be the opposite side [1], hypotenuse side [2], or neither? [3]"))
             match asin_choice:
@@ -200,16 +242,22 @@ def asinFunc(optionalValue):
                 case _:
                     oppSide = float(input("Please enter a value for the opposite side: "))
                     hypoSide = float(input("Please enter a value for the hypotenuse side: "))
+        else:
+            oppSide = float(input("Please enter a value for the opposite side: "))
+            hypoSide = float(input("Please enter a value for the hypotenuse side: "))
         asinSolution = float(math.asin(oppSide/hypoSide))
 
         print("The theta (θ) of ", oppSide, " and ", hypoSide, " is ", asinSolution, " radians.")
 
+        optionalValue = asinSolution
+
         user_continue = input("Would you like to continue? Y or N\n")
         if user_continue == "N":
-            break;
+            return asinSolution
 
 def atanFunc(optionalValue):
-    while True:
+    user_continue = "Y"
+    while user_continue == "Y":
         if optionalValue != 0.0:
             atan_choice = int(input("Do you want the previous value to be the opposite side [1], adjacent side [2], or neither? [3]"))
             match atan_choice:
@@ -222,16 +270,22 @@ def atanFunc(optionalValue):
                 case _:
                     oppSide = float(input("Please enter a value for the opposite side: "))
                     adjSide = float(input("Please enter a value for the adjacent side: "))
+        else:
+            oppSide = float(input("Please enter a value for the opposite side: "))
+            adjSide = float(input("Please enter a value for the adjacent side: "))
         atanSolution = float(math.atan(oppSide/adjSide))
 
         print("The theta (θ) of ", oppSide, " and ", adjSide, " is ", atanSolution, " radians.")
 
+        optionalValue = atanSolution
+
         user_continue = input("Would you like to continue? Y or N\n")
         if user_continue == "N":
-            break;
+            return atanSolution
 
 def asecFunc(optionalValue):
-    while True:
+    user_continue = "Y"
+    while user_continue == "Y":
         if optionalValue != 0.0:
             asec_choice = int(input("Do you want the previous value to be the adjacent side [1], hypotenuse side [2], or neither? [3]"))
             match asec_choice:
@@ -244,16 +298,22 @@ def asecFunc(optionalValue):
                 case _:
                     adjSide = float(input("Please enter a value for the adjacent side: "))
                     hypoSide = float(input("Please enter a value for the hypotenuse side: "))
+        else:
+            adjSide = float(input("Please enter a value for the adjacent side: "))
+            hypoSide = float(input("Please enter a value for the hypotenuse side: "))
         asecSolution = float(1/math.acos(adjSide/hypoSide))
 
         print("The theta (θ) of ", adjSide, " and ", hypoSide, " is ", asecSolution, " radians.")
 
+        optionalValue = asecSolution
+
         user_continue = input("Would you like to continue? Y or N\n")
         if user_continue == "N":
-            break;
+            return asecSolution
 
 def acscFunc(optionalValue):
-    while True:
+    user_continue = "Y"
+    while user_continue == "Y":
         if optionalValue != 0.0:
             acsc_choice = int(input("Do you want the previous value to be the opposite side [1], hypotenuse side [2], or neither? [3]"))
             match acsc_choice:
@@ -266,16 +326,22 @@ def acscFunc(optionalValue):
                 case _:
                     oppSide = float(input("Please enter a value for the opposite side: "))
                     hypoSide = float(input("Please enter a value for the hypotenuse side: "))
+        else:
+            oppSide = float(input("Please enter a value for the opposite side: "))
+            hypoSide = float(input("Please enter a value for the hypotenuse side: "))
         acscSolution = float(1/math.asin(oppSide/hypoSide))
 
         print("The theta (θ) of ", oppSide, " and ", hypoSide, " is ", acscSolution, " radians.")
 
+        optionalValue = acscSolution
+
         user_continue = input("Would you like to continue? Y or N\n")
         if user_continue == "N":
-            break;
+            return acscSolution
 
 def acotFunc(optionalValue):
-    while True:
+    user_continue = "Y"
+    while user_continue == "Y":
         if optionalValue != 0.0:
             acot_choice = int(input("Do you want the previous value to be the opposite side [1], adjacent side [2], or neither? [3]"))
             match acot_choice:
@@ -288,39 +354,46 @@ def acotFunc(optionalValue):
                 case _:
                     oppSide = float(input("Please enter a value for the opposite side: "))
                     adjSide = float(input("Please enter a value for the adjacent side: "))
+        else:
+            oppSide = float(input("Please enter a value for the opposite side: "))
+            adjSide = float(input("Please enter a value for the adjacent side: "))
         acotSolution = float(1/math.atan(oppSide/adjSide))
 
         print("The theta (θ) of ", oppSide, " and ", adjSide, " is ", acotSolution, " radians.")
 
+        optionalValue = acotSolution
+
         user_continue = input("Would you like to continue? Y or N\n")
         if user_continue == "N":
-            break;
+            return acotSolution
 
 if __name__ == "__main__":
     # Rework these so that the pass-in argument isn't a constant.
-    user_choice = int(trigCompPrompt())
-    match user_choice:
-        case 1:
-            cosFunc(1)
-        case 2:
-            sinFunc(1)
-        case 3:
-            tanFunc(1)
-        case 4:
-            secFunc(1)
-        case 5:
-            cscFunc(1)
-        case 6:
-            cotFunc(1)
-        case 7:
-            acosFunc(1)
-        case 8:
-            asinFunc(1)
-        case 9:
-            atanFunc(1)
-        case 10:
-            asecFunc(1)
-        case 11:
-            acscFunc(1)
-        case 12:
-            acotFunc(1)
+    passAroundVal = 0.0
+    while True:
+        user_choice = int(trigCompPrompt())
+        match user_choice:
+            case 1:
+                passAroundVal = cosFunc(passAroundVal)
+            case 2:
+                passAroundVal = sinFunc(passAroundVal)
+            case 3:
+                passAroundVal = tanFunc(passAroundVal)
+            case 4:
+                passAroundVal = secFunc(passAroundVal)
+            case 5:
+                passAroundVal = cscFunc(passAroundVal)
+            case 6:
+                passAroundVal = cotFunc(passAroundVal)
+            case 7:
+                passAroundVal = acosFunc(passAroundVal)
+            case 8:
+                passAroundVal = asinFunc(passAroundVal)
+            case 9:
+                passAroundVal = atanFunc(passAroundVal)
+            case 10:
+                passAroundVal = asecFunc(passAroundVal)
+            case 11:
+                passAroundVal = acscFunc(passAroundVal)
+            case 12:
+                passAroundVal = acotFunc(passAroundVal)
